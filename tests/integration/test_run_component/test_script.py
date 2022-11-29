c47921a1e1ced81af9b9a1122fe5c8b3ed28a653
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2022-11-29 16:26:07 +0100

    Fix integration test script formatting

--- tests/integration/test_run_component/test_script.py
+++ tests/integration/test_run_component/test_script.py
@@ -1,10 +1,12 @@
 import pytest
 
+
 def test_run_component(run_component):
     run_component(["--output", "bar.txt"])
-    with open("bar.txt", 'r') as open_output_file:
+    with open("bar.txt", "r") as open_output_file:
         contents = open_output_file.read()
         assert contents == "foo!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pytest.main([__file__], plugins=["viashpy"])
