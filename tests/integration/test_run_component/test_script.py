f4341ba69f819eb2c09860345845d02c0932d7f4
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2023-02-03 13:54:17 +0100

    Return captured stderr and stdout in run_component (#7)
    * Return captured stderr and stdout in run_component
    
    * Add check for actual message in test
    
    * Fix formatting

--- tests/integration/test_run_component/test_script.py
+++ tests/integration/test_run_component/test_script.py
@@ -1,12 +1,17 @@
 import pytest
+import sys
 
 
 def test_run_component(run_component):
-    run_component(["--output", "bar.txt"])
+    captured_output = run_component(["--output", "bar.txt"])
     with open("bar.txt", "r") as open_output_file:
         contents = open_output_file.read()
         assert contents == "foo!"
+    assert (
+        captured_output is not None
+    ), "Some output from stdout or stderr should have been captured"
+    assert b"This is a logging statement" in captured_output
 
 
 if __name__ == "__main__":
-    pytest.main([__file__], plugins=["viashpy"])
+    sys.exit(pytest.main([__file__], plugins=["viashpy"]))
